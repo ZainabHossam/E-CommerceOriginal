7345c598078a804945f58cb20c6ac907187c1dff
ZainabHossam <zainabhossam94@gmail.com>
2022-08-05 12:41:11 +0200

    Runners

---
+++ src/test/java/org/example/TestRunner/runners.java
@@ -0,0 +1,19 @@
+package org.example.TestRunner;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/main/resources/features",
+        //glue related to Step definitions
+        glue =    {"org/example/StepDefs"},
+        plugin = { "pretty",
+                "html:target/cucumber.html",
+                "json:target/cucumber.json",
+                "junit:target/cukes.xml",
+                "rerun:target/rerun.txt"},
+
+        tags = "@smoke"
+
+)
+public class runners extends AbstractTestNGCucumberTests  {
+}
