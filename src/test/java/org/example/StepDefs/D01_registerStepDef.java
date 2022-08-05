f383ab794f2b2d00ec3fd0cdf2d374639fb2e30a
ZainabHossam <zainabhossam94@gmail.com>
2022-08-05 09:06:58 +0200

    Initial commit

---
+++ src/test/java/org/example/StepDefs/D01_registerStepDef.java
@@ -0,0 +1,10 @@
+package org.example.StepDefs;
+
+import io.cucumber.java.en.Given;
+
+public class D01_registerStepDef {
+    @Given("user go to register page")
+    public void goRegisterPage(){
+        System.out.println("Hii Test");
+    }
+}
